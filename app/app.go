b20bbaf6a72dfbd81e525b9c5053c36e48542e5e
Patrick Navega <psnavega97@icloud.com>
2023-05-07 00:26:10 -0300

    feat: create search ips function

--- app/app.go
+++ app/app.go
@@ -1,11 +1,45 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "CLI application"
 	app.Usage = "Search ips and server names"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search ips at internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, error := net.LookupIP(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
